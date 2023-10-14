8b04c6f86edcd6798163b9600d7d73e52d93ce4f
milijabar <milijabar@gmail.com>
2023-10-14 16:10:20 +0300

    Added symbol counter and result printing in TextView

--- app/src/main/java/com/example/mipt_lab2/MainActivity.java
+++ app/src/main/java/com/example/mipt_lab2/MainActivity.java
@@ -3,12 +3,32 @@ package com.example.mipt_lab2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText edUserInput;
+    Button btnCount;
+    TextView tvMain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.edUserInput = findViewById(R.id.edUserInput);
+        this.btnCount = findViewById(R.id.btnCount);
+        this.tvMain = findViewById(R.id.tvMain);
+    }
+
+    public void onBtnCountClick(View view) {
+        int result = TextCounter.countSymbols(this.edUserInput.getText().toString());
+        Log.i("CountResult", String.valueOf(result));
+
+        this.tvMain.setText(String.valueOf(result));
     }
 }
---
+++ app/src/main/java/com/example/mipt_lab2/TextCounter.java
@@ -0,0 +1,7 @@
+package com.example.mipt_lab2;
+
+public class TextCounter {
+    public static int countSymbols(String phrase){
+        return phrase.length();
+    }
+}
